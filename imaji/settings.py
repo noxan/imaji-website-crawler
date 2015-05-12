a3f8c1d2e8173db56d0e554fad768998e8d30185
noxan <noxan@byteweaver.net>
2015-05-12 18:15:47 +0200

    create scrapy project

---
+++ imaji/settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+# Scrapy settings for imaji project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'imaji'
+
+SPIDER_MODULES = ['imaji.spiders']
+NEWSPIDER_MODULE = 'imaji.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'imaji (+http://www.yourdomain.com)'
